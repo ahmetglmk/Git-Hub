0604119075c7b263606f72cf2158ef017f09619a
Ahmet Gulmek <ahmetgulmek799@gmail.com>
2023-12-27 00:57:48 +0300

    v 2.6

---
+++ src/C06_SixthClass.java
@@ -0,0 +1,9 @@
+public class C06_SixthClass {
+    public static void main(String[] args) {
+
+        String isim = "Ahmet";
+        String soyIsim = "Gülmek";
+
+        System.out.println(isim+" "+soyIsim);
+    }
+}
