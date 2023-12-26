da9b35093f43cc700e5e2cd179e52e82fedd91a8
Ahmet Gulmek <ahmetgulmek799@gmail.com>
2023-12-26 23:20:21 +0300

    C01_FirstClass

--- src/C01_FirstClass.java
+++ src/C01_FirstClass.java
@@ -2,5 +2,6 @@ public class C01_FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Hello Git-Hub, Hello Java");
+        System.out.println("Merhaba Git-Hub, Merhaba Java");
     }
 }
