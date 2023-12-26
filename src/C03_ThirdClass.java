c027a6ada49a6777e3b56e0e9b6837b3bc8eae51
Ahmet Gulmek <ahmetgulmek799@gmail.com>
2023-12-27 00:50:20 +0300

    C03_ThirdClass

---
+++ src/C03_ThirdClass.java
@@ -0,0 +1,11 @@
+public class C03_ThirdClass {
+    public static void main(String[] args) {
+
+        int baslangic = 0;
+
+        while (baslangic < 100){
+            baslangic +=2;
+            System.out.print(baslangic + " ");
+        }
+    }
+}
